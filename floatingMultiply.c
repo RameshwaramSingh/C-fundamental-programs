bca26351207839fb896f91a509152227229543ac
RameshwaramSingh <156481890+RameshwaramSingh@users.noreply.github.com>
2024-09-20 12:13:49 +0530

    Update floatingMultiply.c

--- floatingMultiply.c
+++ floatingMultiply.c
@@ -4,5 +4,6 @@ int main(){
 	float b= 2.64;
 	float c = a*b;
 	printf("Multiplication of floating point %0.1f ",c);
+	printf("Multiplication of floating point two decimal point %0.2f ",c);
 	return 0;
 }
